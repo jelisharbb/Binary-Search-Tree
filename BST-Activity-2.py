9fedc76a609ce7ffb32717e02a10e83402247a8c
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-01-14 14:47:20 +0800

    Defined a class and initialized.

---
+++ BST-Activity-2.py
@@ -0,0 +1,7 @@
+# # Create a binary tree using my full name as the elements
+
+class binarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
