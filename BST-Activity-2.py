2c2eb10b97a1e1d9a168bcbb3bb577121e1b79bb
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-01-14 14:50:16 +0800

    Defined a function that adds a child node.

--- BST-Activity-2.py
+++ BST-Activity-2.py
@@ -5,3 +5,22 @@ class binarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+    def addChild(self, data):
+        if data == self.data:
+            # returns the data if it already exists
+            return
+
+        if data < self.data:
+            # adds data in the left subtree
+            if self.left:
+                self.left.addChild(data)
+            else:
+                self.left = binarySearchTreeNode(data)
+
+        else:
+            # adds data in the right subtree
+            if self.right:
+                self.right.addChild(data)
+            else:
+                self.right = binarySearchTreeNode(data)
